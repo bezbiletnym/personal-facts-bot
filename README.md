05d0c7712295140ec27cc6a77921014e4d465ef3
m.omelyanenko <61245298+bezbiletnym@users.noreply.github.com>
2025-03-12 19:34:25 +0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Facts Bot
+
+I love interesting facts, but I don't like to browse websites from my phone.
+So I make this little project to send articles from https://nowiknow.com/ to my Telegram through a chatbot.
---
+++ main.py
@@ -0,0 +1,19 @@
+import requests, bs4, yaml
+
+def read_config() -> dict:
+    config_data = {}
+    try:
+        with open('config.yaml') as f:
+            config_data = yaml.safe_load(f)
+    except Exception as err:
+        print(f"Unable to read the config file: {repr(err)}")
+    return config_data
+
+def send_tg_message(token='', chat_id=0, message='') -> bool:
+    x = requests.get(
+        f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}')
+    return x.ok
+
+config = read_config()
+print(send_tg_message(token=config.get('BOT_TOKEN', ''), chat_id=config.get('CHAT_ID', 0), message='br'))
+
